64dbf30e62b9d99ad769616a89de8c0397c2f378
Juan Mendoza <jsmendozap@unal.edu.co>
2024-04-05 13:04:43 -0500

    Setting lib path

--- App.bat
+++ App.bat
@@ -1,9 +1,10 @@
 @echo off
 
 rem Setting the path to the Rscript executable
-PATH C:\Program Files\R\R-4.3.3\bin\x64
+PATH C:\Program Files\R\R-4.2.3\bin\x64
 
 rem Executing Shiny applicaion from GitHub
-Rscript -e "if(!rlang::is_installed('shiny')) { install.packages('shiny') }"
+Rscript -e "path <- paste0('~/R/win-library/', getRversion()); if(!dir.exists(path)) {dir.create(path, recursive = T); Sys.setenv(R_LIBS_USER = path)}"
+Rscript -e "if(is.na(match('shiny', installed.packages()))) { .libPaths(Sys.getenv('R_LIBS_USER')); install.packages('shiny', repos = 'http://cran.us.r-project.org') }"
 Rscript -e "shiny::runGitHub('dashboard-reactor', 'jsmendozap', ref = 'main', launch.browser = T)"
 pause
