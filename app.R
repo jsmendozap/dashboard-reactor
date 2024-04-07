ee0df4b9463f300af4e8402c1bf61782b6be8261
Juan Mendoza <jsmendozap@unal.edu.co>
2024-04-07 08:11:43 -0500

    Update app.R

--- app.R
+++ app.R
@@ -2,7 +2,6 @@
 
 if (!"pak" %in% installed.packages()) { install.packages("pak", repos = 'http://cran.us.r-project.org') }
 
-cat("Veryfing and installing required packages")
 lib <- c("tidyverse", "readxl", "janitor", "DT", "dygraphs", "bs4Dash", "shiny", "prettyunits", "plotly")
 lapply(lib, \(x) { 
     if(rlang::is_installed(x)) {
@@ -13,6 +12,5 @@ lapply(lib, \(x) {
     }
 }
 
-cat("Running application")
 walk(.x = c("src/ui.R", "src/server.R"), .f = source)
 shinyApp(ui, server)
