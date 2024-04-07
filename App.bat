dc25ffc294aba115a74acf98a439f39a9b35acd0
Juan Mendoza <jsmendozap@unal.edu.co>
2024-04-07 14:23:54 +0200

    Creating .Rprofile file to set libraries location

--- App.bat
+++ App.bat
@@ -1,10 +1,16 @@
 @echo off
 
 rem Setting the path to the Rscript executable
-PATH C:\Program Files\R\R-4.2.3\bin\x64
+PATH C:\Program Files\R\R-4.3.3\bin\x64
 
-rem Executing Shiny applicaion from GitHub
-Rscript -e "path <- paste0('~/R/win-library/', getRversion()); if(!dir.exists(path)) {dir.create(path, recursive = T); Sys.setenv(R_LIBS_USER = path)}"
-Rscript -e "if(is.na(match('shiny', installed.packages()))) { .libPaths(Sys.getenv('R_LIBS_USER')); install.packages('shiny', repos = 'http://cran.us.r-project.org') }"
+rem Setting libraries location 
+Rscript -e "path <- paste0('~/R/win-library/', getRversion()); cat(paste0('.libPaths(\"', path, '\")'), sep = '\n', file = '~/.Rprofile', append = TRUE)"
+Rscript -e "if(!dir.exists(.libPaths()[1])) { dir.create(.libPaths()[1], recursive = T) }"
+
+rem Installing shiny if required
+Rscript -e "if(is.na(match('shiny', installed.packages()))) { install.packages('shiny', repos = 'http://cran.us.r-project.org') }"
+
+echo Executing Shiny application from GitHub
 Rscript -e "shiny::runGitHub('dashboard-reactor', 'jsmendozap', ref = 'main', launch.browser = T)"
+
 pause
