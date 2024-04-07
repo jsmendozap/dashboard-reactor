615005b74fb42f8505ba4ab882eb5aa877369be3
Juan Mendoza <jsmendozap@unal.edu.co>
2024-04-07 15:30:09 +0200

    Update app.R

--- app.R
+++ app.R
@@ -1,8 +1,8 @@
 #!/usr/bin/env Rscript
 
+cat("Verifying and installing required packages\n")
 if (!"pak" %in% installed.packages()) { install.packages("pak", repos = 'http://cran.us.r-project.org') }
 
-cat("Verifying and installing required packages\n")
 lib <- c("tidyverse", "readxl", "janitor", "DT", "dygraphs", "bs4Dash", "shiny", "prettyunits", "plotly")
 lapply(lib, \(x) { 
     if(rlang::is_installed(x)) {
