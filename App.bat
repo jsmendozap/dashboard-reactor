af69ca6600284a764aa46e36b6a433a5967a0ea0
Juan Mendoza <jsmendozap@unal.edu.co>
2024-04-07 15:04:39 +0200

    Update App.bat

--- App.bat
+++ App.bat
@@ -4,8 +4,8 @@ rem Setting the path to the Rscript executable
 PATH C:\Program Files\R\R-4.3.3\bin\x64
 
 rem Setting libraries location 
+Rscript -e "path <- paste0('~/R/win-library/', getRversion()); if(!dir.exists(path)) { dir.create(path, recursive = T) }"
 Rscript -e "path <- paste0('~/R/win-library/', getRversion()); cat(paste0('.libPaths(\"', path, '\")'), sep = '\n', file = '~/.Rprofile', append = TRUE)"
-Rscript -e "if(!dir.exists(.libPaths()[1])) { dir.create(.libPaths()[1], recursive = T) }"
 
 rem Installing shiny if required
 Rscript -e "if(is.na(match('shiny', installed.packages()))) { install.packages('shiny', repos = 'http://cran.us.r-project.org') }"
