95327af7e182f0cc08bdcc322cb164279a36b827
Juan Mendoza <jsmendozap@unal.edu.co>
2024-04-08 13:37:29 +0200

    Update app.R

--- app.R
+++ app.R
@@ -1,17 +1,14 @@
 #!/usr/bin/env Rscript
 
 cat("Verifying and installing required packages\n")
-if (!"pak" %in% installed.packages()) { install.packages("pak", repos = 'http://cran.us.r-project.org') }
 
-lib <- c("tidyverse", "readxl", "janitor", "DT", "dygraphs", "bs4Dash", "shiny", "prettyunits", "plotly")
-sapply(lib, \(x) { 
-    if(rlang::is_installed(x)) {
-        require(x, character.only = T, quietly = T, warn.conflicts = F)
-    } else {
-        pak::pkg_install(x)
-        require(x, character.only = T, quietly = T, warn.conflicts = F)
-    }
-})
+if (!"pacman" %in% installed.packages()) { install.packages("pacman") }
+if (!"pacman" %in% installed.packages()) { install.packages("pacman", repos = 'http://cran.us.r-project.org') }
+
+pacman::p_load(
+    tidyverse, readxl, janitor, DT, dygraphs,
+    bs4Dash, shiny, prettyunits, plotly
+)
 
 cat("Running application\n")
 walk(.x = c("src/ui.R", "src/server.R"), .f = source)
