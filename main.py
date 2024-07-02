96cee1aa0f0d4a358dc80bac519b90974d53761e
Juan Mendoza <jsmendozap@unal.edu.co>
2024-07-01 20:50:10 -0500

    Deleting App.bat file

--- main.py
+++ main.py
@@ -1,35 +1,35 @@
-import subprocess
-import os 
-import sys
+from subprocess import check_call, run, Popen
+from os import path, environ, makedirs
+from sys import executable
 
 try:
-    import winapps
+    from winapps import list_installed
 except ImportError:
-    subprocess.check_call([sys.executable, '-m', 'pip', 'install', 'winapps'])
-    import winapps
+    check_call([executable, '-m', 'pip', 'install', 'winapps'])
+    from winapps import list_installed
 
-for item in winapps.list_installed():
+for item in list_installed():
     if "R for Windows" in item.name:
         R = f"{item.install_location}\\bin\\x64\\Rscript.exe"
 
-app = "shiny::runGitHub('dashboard-reactor', 'jsmendozap', ref = 'main', launch.browser = T)"
-
 # Setting libraries location
-Rversion = subprocess.run(f"{R} -e \"getRversion() |> as.character() |> cat()\"", capture_output = True, text = True).stdout.strip()
-Rlib = os.path.join(os.environ["USERPROFILE"], f"Documents\\R\\win-library\\{Rversion}")
+Rversion = run(f"{R} -e \"getRversion() |> as.character() |> cat()\"", capture_output = True, text = True).stdout.strip()
+Rlib = path.join(environ["USERPROFILE"], f"Documents\\R\\win-library\\{Rversion}")
 
-if not os.path.exists(Rlib):
-    os.makedirs(Rlib)
-    path = os.path.join(os.environ["USERPROFILE"], "Documents\\.Rprofile")
+if not path.exists(Rlib):
+    makedirs(Rlib)
+    path = path.join(environ["USERPROFILE"], "Documents\\.Rprofile")
     with open(path, 'a') as Rprofile:
         Rprofile.write(f".libPaths(\"~/R/win-library/{Rversion}\")\n")
 
 shiny = "if(is.na(match('shiny', installed.packages()))) { install.packages('shiny', repos = 'http://cran.us.r-project.org') }"
-subprocess.run(f"{R} -e \"{shiny}\"")
+run(f"{R} -e \"{shiny}\"")
 
 # Verifying quarto installation
-quarto_path = subprocess.run("where quarto.exe", capture_output = True, text = True).stdout.strip()
+quarto_path = run("where quarto.exe", capture_output = True, text = True).stdout.strip()
+
+app = "shiny::runGitHub('dashboard-reactor', 'jsmendozap', ref = 'main', launch.browser = T)"
 command = f'{R} -e "{app}" "{quarto_path}"'
 
 # Executing the application 
-subprocess.Popen(command, shell = True)
+Popen(command, shell = True)
