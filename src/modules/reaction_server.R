e205d94070b2d749d648e819c00f1d2e563e253c
jsmendozap <jsmendozap@unal.edu.co>
2024-08-11 20:11:59 -0500

    create a reaction setting ready

--- src/modules/reaction_server.R
+++ src/modules/reaction_server.R
@@ -1,10 +1,41 @@
 reaction_server <- function(id) {
   moduleServer(id, function(input, output, session){
   
-  output$descrip <- renderUI({
-      req(input$formula)
-
-      
+    ns <- NS(id)
+    table <- reactiveVal(
+      tibble(
+        ID = numeric(),
+        Compound = character(),
+        Type = character(),
+        MFC_1 = numeric(),
+        MFC_2 = numeric(),
+        MFC_3 = numeric(),
+        MFC_4 = numeric()
+      )
+    )
+    
+    observeEvent(input$new, {
+      table() %>%
+        add_row(
+          ID = 1,
+          Compound = input$compound,
+          Type = input$c_type,
+          MFC_1 = input$mfc1,
+          MFC_2 = input$mfc2,
+          MFC_3 = input$mfc3,
+          MFC_4 = input$mfc4,
+        ) %>%
+        mutate(ID = row_number()) %>%
+        table()
     })
+    
+    output$compounds_table <- renderReactable({
+      table() %>%
+        rename_with(.fn = ~str_replace_all(.x, '_', ' '), .cols = contains('MFC')) %>%
+        custom_reactable(columns = list(Compound = colDef(minWidth = 250)),
+                         style = "border-radius: 3px"
+                        )
+    })
+    
   })
 }
--- src/pages/reaction.R
+++ src/pages/reaction.R
@@ -1,23 +1,35 @@
 reaction_ui <- function(id){
 
   ns <- NS(id)
+  choices <- c("Reactant", "Product", "IS")
 
   tabItem(tabName = 'reaction',
-                fluidRow(
-                  box(title = "Stoichiometric reaction settings",
-                       status = 'info',
-                       solidHeader = T,
-                       width = 12,
-                       searchInput(
-                         inputId = ns("formula"),
-                         label = "Please write the reaction", 
-                         placeholder = "Propileno + co2",
-                         btnSearch = icon("search"), 
-                         btnReset = icon("remove"),
-                         width = "100%"
-                      ),
-                      uiOutput(ns('descrip'))
-                  )
-                )
+          tabBox(width = 12, 
+                tabPanel(title = "Create setting",
+                        fluidRow(
+                          div(
+                            column(width = 4, textInput(ns('compound'), 'Compound')),
+                            column(width = 2, pickerInput(ns('c_type'), 'Type', choices)),
+                            column(width = 1, numericInput(ns("mfc1"), "MFC 1", value = NA)),
+                            column(width = 1, numericInput(ns("mfc2"), "MFC 2", value = NA)),
+                            column(width = 1, numericInput(ns("mfc3"), "MFC 3", value = NA)),
+                            column(width = 1, numericInput(ns("mfc4"), "MFC 4", value = NA)),
+                            column(width = 2, actionButton(ns("new"), "Add row")),
+                            style = "display: flex; align-items: center"               
+                          ),
+                          reactableOutput(ns('compounds_table')),
+                        ),
+                        br(),
+                        searchInput(
+                          inputId = ns("save"),
+                          label = "Save setting", 
+                          placeholder = "Setting name",
+                          btnSearch = icon("floppy-disk"), 
+                          width = "450px"
+                        )
+                ),
+                tabPanel(title = "Load Setting"
+                )      
+          )
         )
 }
